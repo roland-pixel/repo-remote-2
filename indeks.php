52d63830d2df85ebfc6ec0ba682eb020d85c308a
roland-pixel <kharis7181@gmail.com>
2024-08-08 18:34:58 +0700

    menambahkan list

--- indeks.php
+++ indeks.php
@@ -8,5 +8,8 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae architecto deserunt, possimus fugiat velit eveniet quisquam non culpa nesciunt asperiores repudiandae perferendis cum, dolor neque voluptatibus quidem nisi voluptatem illum?</p>
+    <ul>
+        <li>anjay</li>
+    </ul>
 </body>
 </html>
